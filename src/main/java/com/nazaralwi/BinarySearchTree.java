550f2ea421c16e2fdc8d359a7685fdafa84abdab
Nazar Alwi <alwinazar75@gmail.com>
2021-06-25 08:59:21 +0700

    Add `insert(Node input)` method for inserting the data

--- src/main/java/com/nazaralwi/BinarySearchTree.java
+++ src/main/java/com/nazaralwi/BinarySearchTree.java
@@ -6,4 +6,35 @@ public class BinarySearchTree {
     boolean isEmpty() {
         return root == null;
     }
+
+    void insert(Node input) {
+        Node temp = root;
+        Node parent = null;
+        boolean flag = false;
+        if (isEmpty()) {
+            root = input;
+        } else {
+            while (temp != null) {
+                parent = temp;
+                if (input.data < temp.data) {
+                    temp = temp.left;
+                    flag = true;
+                } else if (input.data > temp.data) {
+                    temp = temp.right;
+                    flag = true;
+                } else {
+                    System.out.printf("Duplicate data not allowed");
+                    break;
+                }
+            }
+
+            if (flag) {
+                if (input.data > parent.data) {
+                    parent.right = input;
+                } else if (input.data < parent.data) {
+                    parent.left = input;
+                }
+            }
+        }
+    }
 }
