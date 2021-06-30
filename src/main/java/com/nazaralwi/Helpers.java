c2c91092e82b01a089bd8020feb1d17e6751e05b
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2021-06-30 15:00:06 +0700

    Add `main()` method for user interaction with console

--- src/main/java/com/nazaralwi/Helpers.java
+++ src/main/java/com/nazaralwi/Helpers.java
@@ -1,2 +1,80 @@
-package com.nazaralwi;public class Helpers {
+package com.nazaralwi;
+
+import java.util.Scanner;
+
+public class Helpers {
+    public static void printCommandList() {
+        System.out.println("Menu Stack");
+        System.out.println("1. Insert data");
+        System.out.println("2. Cari data");
+        System.out.println("3. Cari data terbesar");
+        System.out.println("4. Cari data terkecil");
+        System.out.println("5. Hapus data");
+        System.out.println("6. Hapus seluruh data");
+        System.out.println("7. Cari direktori");
+        System.out.println("8. Hitung jumlah leaf");
+        System.out.println("9. Print secara preorder");
+        System.out.println("10. Print secara inorder");
+        System.out.println("11. Print secara postorder");
+        System.out.println("12. Exit");
+    }
+
+    public static void callInsert(Scanner scanner, BinarySearchTree tree) {
+        System.out.print("Masukkan jumlah data: ");
+        int dataCount = scanner.nextInt();
+        for (int i = 0; i < dataCount; i++) {
+            System.out.print("Masukkan data: ");
+            int data = scanner.nextInt();
+            tree.insert(new Node(data));
+        }
+    }
+
+    public static void callFind(Scanner scanner, BinarySearchTree tree) {
+        System.out.print("Data yang dicari: ");
+        int data = scanner.nextInt();
+        tree.find(data);
+    }
+
+    public static void callFindMax(BinarySearchTree tree) {
+        System.out.println("Nilai maximum : " + tree.findMax().data);
+    }
+
+    public static void callFindMin(BinarySearchTree tree) {
+        System.out.println("Nilai minimum : " + tree.findMin().data);
+    }
+
+    public static void callRemoveAll(BinarySearchTree tree) {
+        tree.removeAll();
+    }
+
+    public static void callRemove(Scanner scanner, BinarySearchTree tree) {
+        System.out.print("Masukkan data yang dihapus: ");
+        int data = scanner.nextInt();
+        tree.remove(data);
+    }
+
+    public static void callFindDirectory(Scanner scanner, BinarySearchTree tree) {
+        System.out.print("Masukkan data: ");
+        int data = scanner.nextInt();
+        tree.findDirectory(data);
+    }
+
+    public static void callGetLeafCount(BinarySearchTree tree) {
+        System.out.println("Jumlah leaf: " + tree.getLeafCount());
+    }
+
+    public static void callPrintPreorder(BinarySearchTree tree) {
+        System.out.println("Urutan secara Preorder: ");
+        tree.printPreorder();
+    }
+
+    public static void callPrintInorder(BinarySearchTree tree) {
+        System.out.println("Urutan secara Inorder: ");
+        tree.printInorder();
+    }
+
+    public static void callPrintPostorder(BinarySearchTree tree) {
+        System.out.println("Urutan secara Postorder: ");
+        tree.printPostorder();
+    }
 }
--- src/main/java/com/nazaralwi/Main.java
+++ src/main/java/com/nazaralwi/Main.java
@@ -2,28 +2,63 @@ package com.nazaralwi;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Helpers {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
+        Scanner charScanner = new Scanner(System.in);
         Scanner scanner = new Scanner(System.in);
+        int input;
+        char isContinue;
 
-        System.out.println("Inserting the data");
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Input data: ");
-            int input = scanner.nextInt();
+        do {
+            printCommandList();
 
-            Node data = new Node(input);
-            tree.insert(data);
-        }
+            System.out.print("Pilih operasi (1-12): ");
+            input = scanner.nextInt();
 
-        System.out.println("Find data with path");
-        System.out.print("Input data: ");
-        int data = scanner.nextInt();
-        tree.findDirectory(data);
+            System.out.println();
+            switch (input) {
+                case 1:
+                    callInsert(scanner, tree);
+                    break;
+                case 2:
+                    callFind(scanner, tree);
+                    break;
+                case 3:
+                    callFindMax(tree);
+                    break;
+                case 4:
+                    callFindMin(tree);
+                    break;
+                case 5:
+                    callRemove(scanner, tree);
+                    break;
+                case 6:
+                    callRemoveAll(tree);
+                    break;
+                case 7:
+                    callFindDirectory(scanner, tree);
+                    break;
+                case 8:
+                    callGetLeafCount(tree);
+                    break;
+                case 9:
+                    callPrintPreorder(tree);
+                    break;
+                case 10:
+                    callPrintInorder(tree);
+                    break;
+                case 11:
+                    callPrintPostorder(tree);
+                    break;
+                default:
+                    System.out.println("Input tidak valid");
+                    break;
+            }
 
-        System.out.println();
-
-        tree.findMin();
-        tree.findMax();
+            System.out.print("\nLanjut? (y/n): ");
+            isContinue = charScanner.next().charAt(0);
+            System.out.println();
+        } while (input != 12 && (isContinue == 'y' || isContinue == 'Y'));
     }
 }
